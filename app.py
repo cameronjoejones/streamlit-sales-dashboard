622e8f0e7450fb1920d8368836092a32ae494f1e
Cameron <125303283+cameronjoejones@users.noreply.github.com>
2023-03-19 15:57:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ import pandas as pd
 import plotly.express as px
 from typing import List
 
-
-page_config = st.set_page_config(
+st.set_page_config(
     page_title="Sales Dashboard",
     page_icon=":bar_chart:",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-hide_menu_style = "<style> footer {visibility: hidden;} </style>"
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-
+st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
 
 @st.cache_data
 def load_data():
@@ -21,37 +18,31 @@ def load_data():
     data['ORDERDATE'] = pd.to_datetime(data['ORDERDATE'])
     return data
 
-
 data = load_data()
-
 
 st.title("📊 Sales Dashboard")
 st.sidebar.header("Filters")
 
-# Date range filter
 start_date = pd.Timestamp(st.sidebar.date_input("Start date", data['ORDERDATE'].min().date()))
 end_date = pd.Timestamp(st.sidebar.date_input("End date", data['ORDERDATE'].max().date()))
+
 filtered_data = data[(data['ORDERDATE'] >= start_date) & (data['ORDERDATE'] <= end_date)]
 
-# Product line filter
-product_lines = st.sidebar.multiselect("Product lines", sorted(data['PRODUCTLINE'].unique()), sorted(data['PRODUCTLINE'].unique()))
-if product_lines:
-    filtered_data = filtered_data[filtered_data['PRODUCTLINE'].isin(product_lines)]
+def filter_data(data, column, values):
+    return data[data[column].isin(values)] if values else data
 
-# Country filter
+product_lines = sorted(data['PRODUCTLINE'].unique())
+selected_product_lines = st.sidebar.multiselect("Product lines", product_lines, product_lines)
+filtered_data = filter_data(filtered_data, 'PRODUCTLINE', selected_product_lines)
+
 selected_countries = st.sidebar.multiselect("Select Countries", data['COUNTRY'].unique())
-if selected_countries:
-    filtered_data = filtered_data[filtered_data['COUNTRY'].isin(selected_countries)]
+filtered_data = filter_data(filtered_data, 'COUNTRY', selected_countries)
 
-
-# Order status filter
 selected_statuses = st.sidebar.multiselect("Select Order Statuses", data['STATUS'].unique())
-if selected_statuses:
-    filtered_data = filtered_data[filtered_data['STATUS'].isin(selected_statuses)]
+filtered_data = filter_data(filtered_data, 'STATUS', selected_statuses)
 
 st.sidebar.info('Created by Cameron [GitHub](https://github.com/cameronjoejones/streamlit-sales-dashboard)')
 
-# Calculate KPIs
 @st.cache_resource
 def calculate_kpis(data: pd.DataFrame) -> List[float]:
     total_sales = data['SALES'].sum()
@@ -59,42 +50,49 @@ def calculate_kpis(data: pd.DataFrame) -> List[float]:
     total_orders = data['ORDERNUMBER'].nunique()
     average_sales_per_order = f"{total_sales/total_orders/1000:.2f}K"
     unique_customers = data['CUSTOMERNAME'].nunique()
-    return [sales_in_m, total_orders, average_sales_per_order, unique_customers ]
-
+    return [sales_in_m, total_orders, average_sales_per_order, unique_customers]
 
 st.header("KPI Metrics")
 kpis = calculate_kpis(filtered_data)
 kpi_names = ["Total Sales", "Total Orders", "Average Sales per Order", "Unique Customers"]
 
-col1, col2, col3, col4 = st.columns(4)
-for i, (kpi_name, kpi_value) in enumerate(zip(kpi_names, kpis)):
-    col = col1 if i % 4 == 0 else col2 if i % 4 == 1 else col3 if i % 4 == 2 else col4
-    # delta = round((kpi_value - kpis[i-1])/kpis[i-1]*100, 2) if i > 0 else 0
+for i, (col, (kpi_name, kpi_value)) in enumerate(zip(st.columns(4), zip(kpi_names, kpis))):
     col.metric(label=kpi_name, value=kpi_value)
 
-
-# st.header("Sales by Product Line Over Time")
+combined_data = filtered_data.groupby(['ORDERDATE', 'PRODUCTLINE'])['SALES'].sum().reset_index()
 sales_by_product_line_over_time = filtered_data.groupby(['ORDERDATE', 'PRODUCTLINE'])['SALES'].sum().reset_index()
-fig = px.area(sales_by_product_line_over_time, x='ORDERDATE', y='SALES', color='PRODUCTLINE', 
-              title="Sales by Product Line Over Time", width=900, height=500)
-st.plotly_chart(fig)
+
+combine_product_lines = st.checkbox("Combine Product Lines", value=True)
+
+if combine_product_lines == True:
+    fig = px.area(combined_data, x='ORDERDATE', y='SALES',
+                    title="Sales by Product Line Over Time", width=900, height=500)
+    fig.update_layout(margin=dict(l=20, r=20, t=50, b=20))
+    fig.update_xaxes(rangemode='tozero', showgrid=False)
+    fig.update_yaxes(rangemode='tozero', showgrid=True)
+    st.plotly_chart(fig, use_container_width=True)
+else:
+    fig = px.area(sales_by_product_line_over_time, x='ORDERDATE', y='SALES', color='PRODUCTLINE', 
+                title="Sales by Product Line Over Time", width=900, height=500)
+    fig.update_layout(margin=dict(l=20, r=20, t=50, b=20))
+    fig.update_xaxes(rangemode='tozero', showgrid=False)
+    fig.update_yaxes(rangemode='tozero', showgrid=True)
+    st.plotly_chart(fig, use_container_width=True)
+
 
 
 col1, col2, col3 = st.columns(3)
 
-# Top 10 customers
 with col1:
     st.subheader("Top 10 Customers")
     top_customers = filtered_data.groupby('CUSTOMERNAME')['SALES'].sum().reset_index().sort_values('SALES', ascending=False).head(10)
     st.write(top_customers)
 
-# Top 10 products by sales
 with col2:
     st.subheader("Top 10 Products by Sales")
     top_products = filtered_data.groupby(['PRODUCTCODE', 'PRODUCTLINE'])['SALES'].sum().reset_index().sort_values('SALES', ascending=False).head(10)
     st.write(top_products)
 
-# Total sales by product line
 with col3:
     st.subheader("Total Sales by Product Line")
     total_sales_by_product_line = filtered_data.groupby('PRODUCTLINE')['SALES'].sum().reset_index()
